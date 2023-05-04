0165138ed6336150e0a4dbfe12164e75268ff245
HyunHo Lee <dlgusgh2001@naver.com>
2023-05-04 23:53:05 +0900

    ♻️ 데이터 패치 로직 개선

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,45 @@
 import styled from '@emotion/styled';
 import { useQuery } from '@tanstack/react-query';
-import axios, { AxiosError, Method } from 'axios';
+import axios, { Method } from 'axios';
 import type { NextPage } from 'next';
 import { useEffect } from 'react';
 
+type MovieResponseModel = {
+  dates: {
+    maximum: string;
+    minimum: string;
+  };
+  page: number;
+  results: any[];
+  total_pages: number;
+  total_results: number;
+};
+
 const HomePage: NextPage = () => {
-  const fetchMovies = async () => {
-    try {
-      const res = await axios({
-        method: 'get' as Method,
-        url: `api/movie/now_playing?api_key=${process.env.NEXT_PUBLIC_THEMOVIEDB_APIKEY}`,
-      });
-      return res;
-    } catch (error) {
-      const err = error as AxiosError;
-      return err;
-    }
+  const fetchMovies = async (): Promise<MovieResponseModel> => {
+    const res = await axios({
+      method: 'get' as Method,
+      url: `api/movie/now_playing?api_key=${process.env.NEXT_PUBLIC_THEMOVIEDB_APIKEY}`,
+    });
+    return res.data;
   };
 
-  const { data, isLoading } = useQuery({
+  const { data } = useQuery<MovieResponseModel>({
     queryKey: ['movie'],
     queryFn: fetchMovies,
+    onSuccess: () => {
+      // 성공시 처리
+    },
+    onError: () => {
+      // 실패시 처리
+    },
   });
 
   useEffect(() => {
     if (data) {
       console.log(data);
     }
-  }, [isLoading]);
+  }, [data]);
 
   return <Container>Home</Container>;
 };
